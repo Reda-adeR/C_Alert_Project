321bbbe6824344dd2c3d925ee4b18d7633aa67a2
Ano <useranonyme317@gmail.com>
2025-06-24 17:05:24 +0100

    health check added

--- cropalert_backend/cropalert/urls.py
+++ cropalert_backend/cropalert/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
     path('alerts/', include('alert.urls')),
+    path("health/", lambda request: JsonResponse({"status": "ok"})),
+
 ]
